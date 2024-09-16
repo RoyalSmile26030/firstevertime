9126a93a656e64f92447f309dbf505c786cc2bdf
RoyalSmile26030 <nietrestamichal@gmail.com>
2024-09-16 17:00:10 -0400

    hello world

--- src/demo/monday/evining/DemoMondayEvining.java
+++ src/demo/monday/evining/DemoMondayEvining.java
@@ -13,7 +13,8 @@ public class DemoMondayEvining {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+   {System.out.println("helloworld");
         // TODO code application logic here
     }
     //change 
